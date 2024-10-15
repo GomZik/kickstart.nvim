c8619fe7630993307cf381027cbbb9c2b0ea779f
Aliaksiej Homza <aliaksiej.homza@yomobile.com>
2024-10-15 09:33:15 +0200

    Use official nord theme

--- init.lua
+++ init.lua
@@ -820,12 +820,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'shaunsingh/nord.nvim',
-    --'nordtheme/vim',
+    --'shaunsingh/nord.nvim',
+    'nordtheme/vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      require('nord').set()
-      --vim.cmd.colorscheme 'nord'
+      --require('nord').set()
+      vim.cmd.colorscheme 'nord'
 
       -- You can configure highlights by doing something like:
     end,
